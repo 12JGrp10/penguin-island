72723034561a8a0e8d014d419abc6885944f8082
12JGrp10 <12jgrp10@gmail.com>
2021-01-18 11:07:42 +0800

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -7,34 +7,7 @@ import { NavController } from 'ionic-angular';
   styleUrls: ['home.css']
 })
 export class HomePage {
-  sex: string;
-  height: number;
-  weight: number;
-  age: number;
-  Calories: number;
-  total: number;
-  activity: number;
-  CaloriesMessage: string;
-  
 
-  constructor(public navCtrl: NavController) {}
-  
-
-  calculateCalories() {
-
-
-    if (this.sex === "m") { 
-    console.log("hello world");
-      this.Calories = (10 * this.weight + 6.25 * this.height - 5 * this.age + 5);
-    }
-    else if (this.sex === "f") { 
-      this.Calories = (10 * this.weight + 6.25 * this.height - 5 * this.age - 161);
-    } 
-    else{
-      this.Calories = 0.01;
-    }
-    this.total = parseFloat((this.Calories * this.activity).toFixed(2));
 
     }
   
-    }
